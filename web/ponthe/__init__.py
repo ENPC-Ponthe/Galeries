16dc96954968d05848c22e58d0ef54234a303961
Philippe Ferreira De Sousa <philippe@fdesousa.fr>
2018-03-25 01:03:35 +0100

    move templates

--- web/ponthe/__init__.py
+++ web/ponthe/__init__.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('ponthe.cfg')
 
-from ponthe.users.views import users_blueprint
-from ponthe.files.views import files_blueprint
+from ponthe.public.views import public
+from ponthe.users.views import users
 
 # register the blueprints
-app.register_blueprint(users_blueprint)
-app.register_blueprint(files_blueprint)
+app.register_blueprint(public)
+app.register_blueprint(users)
