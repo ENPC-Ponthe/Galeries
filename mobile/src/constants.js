21b89dde649d6c85f11333c798b53d05b6b17aee
Philippe Ferreira De Sousa <philippe@fdesousa.fr>
2018-08-19 19:52:46 +0200

    create mobile app

---
+++ mobile/src/constants.js
@@ -0,0 +1,11 @@
+export const API_URL = 'https://ponthe.enpc.org/api/'
+
+export const imageFilters = [
+  "Aucun",
+  "Reconstruction morphologique",
+  "Filtre d'illumination variable",
+  "Binarisation Bredley",
+  "Binarisation Niblack",
+  "Binarisation Sauvola",
+  "Fusion en moyenne",
+]
