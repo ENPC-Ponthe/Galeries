e00a7988a3ebce0485cb2867ea0f45b385888b86
Philippe Ferreira De Sousa <philippe@fdesousa.fr>
2018-08-19 19:52:46 +0200

    create mobile app

---
+++ mobile/src/components/upload/HomeScreen.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { Button } from 'react-native-elements'
+
+export default class HomeScreen extends Component {
+  render() {
+    const { navigate } = this.props.navigation
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
+        <View></View>
+        <Button
+          icon={{
+            name: 'image',
+            size: 40,
+            color: 'white'
+          }}
+          title="PHOTO"
+          onPress={() =>
+            navigate('Image')
+          }
+          titleStyle={{ fontWeight: "700", fontSize: 60 }}
+          buttonStyle={{
+            backgroundColor: "#fec72f",
+            width: 200,
+            height: 70,
+            borderColor: "transparent",
+            borderWidth: 0,
+            borderRadius: 50
+          }}
+        />
+        <Button
+          icon={{
+            name: 'camera',
+            size: 40,
+            color: 'white'
+          }}
+          title="VIDEO"
+          onPress={() =>
+            navigate('Video')
+          }
+          titleStyle={{ fontWeight: "700", fontSize: 60 }}
+          buttonStyle={{
+            backgroundColor: "#fec72f",
+            width: 200,
+            height: 70,
+            borderColor: "transparent",
+            borderWidth: 0,
+            borderRadius: 50
+          }}
+        />
+        <View></View>
+      </View>
+    )
+  }
+}
