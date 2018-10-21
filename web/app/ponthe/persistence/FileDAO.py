a5b2217819925c2f901b206cee83c248caee98e5
Philippe Ferreira De Sousa <philippe.ferreira-de-sousa@illuin.tech>
2018-10-21 13:39:35 +0200

    add file deletion in dashboard and gallery view, add link from dashboard to gallery, add moderation button in gallery view

--- web/app/ponthe/persistence/FileDAO.py
+++ web/app/ponthe/persistence/FileDAO.py
@@ -8,8 +8,16 @@ UPLOAD_FOLDER = app.config['MEDIA_ROOT']
 
 class FileDAO:
     @staticmethod
+    def find_by_slug(slug):
+        return File.query.filter_by(slug=slug).one()
+
+    @staticmethod
     def delete(file: File):
         file_path = os.path.join(UPLOAD_FOLDER, file.file_path)
         delete_file(file_path)
         db.session.delete(file)
         db.session.commit()
+
+    @classmethod
+    def delete_by_slug(cls, slug):
+        cls.delete(cls.find_by_slug(slug))
