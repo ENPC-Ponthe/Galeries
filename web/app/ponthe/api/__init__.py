e106f68969a6f47ebe03a3a760e266c4583885cc
Antoine <antoine.eudes@gmail.com>
2018-12-03 18:49:30 +0100

    changed file architecture

--- web/app/ponthe/api/__init__.py
+++ web/app/ponthe/api/__init__.py
@@ -1,11 +1,7 @@
 from flask import Blueprint
-
-# api = Blueprint('api', __name__)
-#
-# from . import routes
 from flask_restplus import Api
 
 api_blueprint = Blueprint('api', __name__)
 api = Api(api_blueprint)
 
-from . import test
+from .public import routes
