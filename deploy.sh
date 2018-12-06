8cc8853336f8b586d05c093a60e8caa21b1fd78c
Philippe Ferreira De Sousa <philippe@fdesousa.fr>
2018-12-06 20:57:18 +0100

    little cleaning

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
-mkdir -p db-backup
 docker-compose exec backup /backup.sh
-docker-compose pull web
-docker-compose up -d web
-docker-compose exec web flask db upgrade
+docker-compose pull
+docker-compose up -d
