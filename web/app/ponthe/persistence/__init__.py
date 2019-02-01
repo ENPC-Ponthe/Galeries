9368f62f45cab227fb049073c9529351b9b479cd
Antoine <antoine.eudes@gmail.com>
2019-02-01 12:01:38 +0100

    added flask_restplus routes

---
+++ web/app/ponthe/persistence/__init__.py
@@ -0,0 +1,6 @@
+from .EventDAO import EventDAO
+from .GalleryDAO import GalleryDAO
+from .YearDAO import YearDAO
+from .FileDAO import FileDAO
+from .CategoryDAO import CategoryDAO
+from .UserDAO import UserDAO
