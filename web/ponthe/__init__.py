c11564d9da23e5246695a5f2db17bc456e37176f
Antoine <antoine.eudes@gmail.com>
2019-02-01 15:02:32 +0100

    added CORS

--- web/ponthe/__init__.py
+++ web/ponthe/__init__.py
@@ -5,6 +5,8 @@ from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_thumbnails import Thumbnail
+from flask_cors import CORS
+
 
 from . import config
 from .logging_config import logging_init
@@ -19,6 +21,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 FlaskCLI(app)
 thumb = Thumbnail(app)
+CORS(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
