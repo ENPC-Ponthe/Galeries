7285d669a8d7509339809b339bb569e15249b791
Antoine <antoine.eudes@gmail.com>
2019-02-13 19:03:12 +0100

    Update routes.py

--- web/app/ponthe/api/private/routes.py
+++ web/app/ponthe/api/private/routes.py
@@ -662,6 +662,8 @@ class MakeGalleryPublic(Resource):
         response = {"msg": "success"}
         return response, 200
 
+
+
 @api.route('/galleries/makeprivate')
 @api.doc(params=    {
                         'gallery_slugs': 'List of slugs of the galleries to be set private'
@@ -690,6 +692,30 @@ class MakeGalleryPublic(Resource):
             return response, 400
         response = {"msg": "success"}
         return response, 200
+
+
+@api.route('/reset-password')
+@api.doc(params=    {
+                        'current_password': 'your current password',
+                        'new_password': 'your new password'
+                    })
+class ResetPasword(Resource):
+    @jwt_check
+    @api.response(200, 'Success')
+    @api.response(400, 'Request incorrect - JSON not valid')
+    @api.response(403, 'Unauthorized - JWT not valid')
+    def post(self):
+        current_password = request.json.get('current_password')
+        new_password = request.json.get('new_password')
+        current_user = UserDAO.get_by_id(get_jwt_identity())
+        if current_user.check_password(current_password):
+            current_user.set_password(new_password)
+            db.session.add(current_user)
+            db.session.commit()
+            return {"msg": "Mot de passe réinitialisé avec succès"}, 200
+        else:
+            return {"msg": "Mot de passe incorrect"}, 400
+
 
 # @api.route('/dashboard')
 # class Dashboard(Resource):
