3aae8ff713463ca55965f1648620ce9e42315202
Thomas <t.chabal@hotmail.fr>
2020-02-17 19:29:43 +0100

    Add contact route for simple messages frontend

--- web/app/ponthe/api/private/routes.py
+++ web/app/ponthe/api/private/routes.py
@@ -87,7 +87,34 @@ class Materiel(Resource):
         msg = Message(subject=f"Demande d'emprunt de {object} par {current_user.firstname} {current_user.lastname}",
                       body=message,
                       sender=f"{current_user.full_name} <no-reply@ponthe.enpc.org>",
-                      recipients=['alexperez3498@hotmail.fr', 'ponthe@liste.enpc.fr'])#['ponthe@liste.enpc.fr'])
+                      recipients=['ponthe@liste.enpc.fr'])
+        mail.send(msg)
+        return {
+            "msg": "Mail envoyé !"
+        }, 200
+
+
+@api.route('/contact')
+@api.doc(params={
+    'device': 'object you would like to borrow to the club',
+    'message': 'your message'
+})
+class Contact(Resource):
+    @api.response(200, 'Success - Mail sent')
+    @api.response(400, 'Request incorrect - JSON not valid')
+    @api.response(403, 'Not authorized - account not valid')
+    def post(self):
+        '''Send a mail to ponthe to borrow material'''
+        message = request.json.get('message')
+        if not message:
+            return {
+                "title": "Erreur - Aucun message",
+                "body": "Veuillez saisir un message"
+            }, 400
+        msg = Message(subject=f"Message de la part de {current_user.firstname} {current_user.lastname}",
+                      body=message,
+                      sender=f"{current_user.full_name} <no-reply@ponthe.enpc.org>",
+                      recipients=['ponthe@liste.enpc.fr'])
         mail.send(msg)
         return {
             "msg": "Mail envoyé !"
