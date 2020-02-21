57285512e139bea9f839fef8c88f09bd93eb866c
thomaschabal <37712675+thomaschabal@users.noreply.github.com>
2020-02-21 19:07:14 +0100

    Restrict access to get-images (#21)

--- web/app/ponthe/api/private/routes.py
+++ web/app/ponthe/api/private/routes.py
@@ -345,7 +345,8 @@ class GetImages(Resource):
                 "body": "Aucune gallerie ne correspond à : " + gallery_slug
             }, 404
 
-        if gallery.private and not GalleryDAO.has_right_on(gallery, current_user):
+        if (gallery.private and not GalleryDAO.has_right_on(gallery, current_user) or 
+                not gallery.private and not GalleryDAO.has_basic_user_right_on(gallery, current_user)):
             return {
                 "title": "Error - Forbidden",
                 "body": "Vous n'avez pas les droits pour accéder à : " + gallery_slug
