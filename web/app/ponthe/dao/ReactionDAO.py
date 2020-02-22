584a484ad46cfe7c2f4ee56871229e8ebce8acd4
Thomas <t.chabal@hotmail.fr>
2020-02-22 02:47:22 +0100

    Debug reactions #3

--- web/app/ponthe/dao/ReactionDAO.py
+++ web/app/ponthe/dao/ReactionDAO.py
@@ -4,12 +4,12 @@ from ..models import Reaction, User, Resource
 class ReactionDAO:
     @staticmethod
     def find_by_slug_and_user(slug: str, user: User):
-        return Reaction.query.join(Resource.slug).filter(Resource.slug == slug, Reaction.user == user).one()
+        return Reaction.query.join(Reaction.resource).filter(Resource.slug == slug, Reaction.user == user).one()
 
     @staticmethod
     def find_all_by_slug(slug: str):
-        return Reaction.query.join(Resource.slug).filter_by(Resource.slug == slug).all()
+        return Reaction.query.join(Reaction.resource).filter_by(Resource.slug == slug).all()
     
     @staticmethod
     def find_all_by_user(user: User):
-        return Reaction.query.filter_by(Reaction.user == user).all()
+        return Reaction.query.filter_by(user=user).all()
