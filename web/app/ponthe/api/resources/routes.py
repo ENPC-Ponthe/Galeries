14806e64490d1a7f203160a157c852fdc39e2623
Thomas <t.chabal@hotmail.fr>
2020-02-27 21:49:25 +0100

    Add route for getting video from slug

--- web/app/ponthe/api/resources/routes.py
+++ web/app/ponthe/api/resources/routes.py
@@ -16,8 +16,7 @@ from ...dao import FileDAO
 ASSET_FOLDER = app.config['ASSET_ROOT']
 
 
-
-@api.route('/get-video')
+@api.route('/get-video/<gallery_slug>')
 @api.doc(params={
     'image_slug': 'the image you want to get the list of reactions'
 })
@@ -25,12 +24,12 @@ class GetVideo(Resource):
     @api.response(200, 'Success')
     @api.response(400, 'Request incorrect - JSON not valid')
     @api.response(403, 'Not authorized - account not valid')
-    def get(self):
+    def get(self, gallery_slug: str):
         '''Get a set of all reactions for a given image_slug'''
         # image_slug = request.json.get("image_slug")
-        video = FileDAO.get_video()
+        video = FileDAO.get_video_from_gallery_slug(gallery_slug)
 
         return send_file(
             open(FileService.get_absolute_file_path(video), "rb"),
-            mimetype='video/mp4'
+            mimetype='video/' + video.extension
             )
