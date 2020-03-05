a6c566a08070741ae3c9788c5e3a47e8e6262d85
Thomas <t.chabal@hotmail.fr>
2020-03-05 17:48:33 +0100

    Add route for getting thumb cover image

--- web/app/ponthe/api/resources/routes.py
+++ web/app/ponthe/api/resources/routes.py
@@ -52,3 +52,25 @@ class GetVideoCoverImage(Resource):
                 )
         else:
             return { "msg": "The video has no cover image" }, 200
+
+
+@api.route('/get-video-cover-image-thumb/<gallery_slug>')
+@api.doc(params={
+    'gallery_slug': 'the video gallery which owns the video you want'
+})
+class GetVideoCoverImage(Resource):
+    @api.response(200, 'Success')
+    @api.response(400, 'Request incorrect - JSON not valid')
+    @api.response(403, 'Not authorized - account not valid')
+    def get(self, gallery_slug: str):
+        '''Get the video cover image in some gallery with slug gallery_slug'''
+        gallery = GalleryDAO().find_by_slug(gallery_slug)
+        cover_image = FileDAO().get_cover_image_of_video_gallery(gallery)
+        if cover_image is not None:
+            thumb_path = FileDAO().get_thumb_path_or_create_it(cover_image)
+            return send_file(
+                open(thumb_path, "rb"),
+                mimetype='image/' + cover_image.extension
+                )
+        else:
+            return { "msg": "The video has no cover image" }, 200
