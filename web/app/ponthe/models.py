be7e989579a2d74b8c31d8e3fbbeeb22ec8955b9
Thomas <t.chabal@hotmail.fr>
2020-03-13 17:38:59 +0100

    Debug videos 1080p #4

--- web/app/ponthe/models.py
+++ web/app/ponthe/models.py
@@ -460,6 +460,7 @@ class File(Resource):
             return f"{self.gallery.slug}/{self.slug}.{self.extension}"
         elif resolution is "720" or resolution is "480" or resolution is "360":
             return f"{self.gallery.slug}/{self.slug}_{resolution}.{self.extension}"
+        return resolution
 
     def __repr__(self):
         return '<File {}>'.format(self.file_path)
